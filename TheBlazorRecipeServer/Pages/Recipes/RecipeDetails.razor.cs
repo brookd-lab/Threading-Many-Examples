4bf5919b371c46e38b428660bd09dc39e481b3f0
David Brook <brookd2022@gmail.com>
2024-10-30 12:04:07 -0700

    add details code behind page

---
+++ TheBlazorRecipeServer/Pages/Recipes/RecipeDetails.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using BlazorRecipeServer.Models;
+using BlazorRecipeServer.Services;
+
+namespace BlazorRecipeServer
+{
+    public class MyRecipeDetails : ComponentBase
+    {
+        [Parameter]
+        public int id { get; set; }
+
+        public Recipe recipe { get; set; }
+
+        [Inject]
+        public IRecipeService _service { get; init; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            recipe = await _service.GetRecipe(id);
+        }
+    }
+}
